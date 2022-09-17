95330d9d7549e4db1fbd300e7a3e6d8e24908ed0
AmanFSD <aman0486@gmail.com>
2022-09-17 09:38:36 +0530

    first commit

---
+++ python01.py
@@ -0,0 +1,5 @@
+name2="Smith"
+roll_no=456
+
+#print(name2)
+print("After Comment")
---
+++ python02.py
@@ -0,0 +1,28 @@
+
+# a=7
+# b=3
+
+# result=a+b
+# print(result)
+# result=a-b
+# print(result)
+# result=a*b
+# print(result)
+# result=a/b
+# print(result)
+# result=a**b
+# print(result)
+# result=a%b
+# print(result)
+
+# result=a//b
+# print(result)
+
+num=9
+
+if num%2==0:
+    print("no is even")
+else:
+    print("no is odd")    
+
+
